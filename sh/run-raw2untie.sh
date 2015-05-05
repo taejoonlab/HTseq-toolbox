38ed16c877dd96d365a27dae8b00d10675fcc6e1
Taejoon Kwon <tkwon@unist.ac.kr>
2015-05-05 21:23:08 +0900

    A script to make clean & untie paired reads.

---
+++ sh/run-raw2untie.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -o pair.o%j
+#SBATCH -J "pair.foo"
+
+#$ -N pair.foo
+
+
+PAIR="$HOME/git/HTseq-toolbox/fastq/raw-to-untie.py"
+
+for R1 in $(ls *_R1.raw.fastq*)
+do
+  R2=${R1/_R1/_R2}
+  OUT=${R1/_R1.raw.fastq.gz/}
+  OUT=${OUT/_R1.raw.fastq/}
+  echo $OUT
+  $PAIR $R1 $R2 $OUT
+done
