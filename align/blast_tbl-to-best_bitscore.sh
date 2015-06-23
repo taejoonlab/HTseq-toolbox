4c83f48593dafb198b95d564cc43dc2df20a0b9c
Taejoon Kwon <tkwon@unist.ac.kr>
2015-06-24 06:30:33 +0900

    Handle gzipped file

--- align/blast_tbl-to-best_bitscore.sh
+++ align/blast_tbl-to-best_bitscore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 BEST="$HOME/git/HTseq-toolbox/align/blast_tbl-to-best_bitscore.py"
-for TBL in $(ls *_tbl)
+for TBL in $(ls *_{tbl,tbl.gz})
 do
   OUT=$TBL"_best"
   if [ ! -e $OUT ]; then
