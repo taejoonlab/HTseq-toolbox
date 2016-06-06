177bfb84740818841bdac1c954ef4a63332f0587
Taejoon Kwon <taejoon.kwon@gmail.com>
2016-06-06 18:38:22 +0900

    Revised bash script to report best bitscore hit

--- align/blast_tbl-to-best_bitscore.sh
+++ align/blast_tbl-to-best_bitscore.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
-BEST="$HOME/git/HTseq-toolbox/align/blast_tbl-to-best_bitscore.py"
-for TBL in $(ls *_{tbl,tbl.gz})
-do
-  OUT=$TBL"_best"
-  if [ ! -e $OUT ]; then
-    echo $TBL
-    $BEST $TBL
-  fi
-done
+DIR="$( dirname "${BASH_SOURCE[0]}" )"
+BEST="$DIR/blast_tbl-to-best_bitscore.py"
+
+TBL_COUNT=`ls -l *_tbl 2>/dev/null | wc -l`
+if [ $TBL_COUNT != 0 ]; then
+  for TBL in $(ls *_tbl)
+  do
+    OUT=$TBL"_best"
+    if [ ! -e $OUT ]; then
+      echo $TBL
+      $BEST $TBL
+    else
+      echo "$OUT exists. Skip."
+    fi
+  done
+else
+  echo "NO file ends with *_tbl. Skip."
+fi
+
+TBL_COUNT=`ls -l *_tbl.gz 2>/dev/null | wc -l`
+if [ $TBL_COUNT != 0 ]; then
+  for TBL in $(ls *_tbl.gz)
+  do
+    OUT=${TBL/.gz$/}"_best"
+    if [ ! -e $OUT ]; then
+      echo $TBL
+      $BEST $TBL
+    else
+      echo "$OUT exists. Skip."
+    fi
+  done
+else
+  echo "NO file ends with *_tbl.gz. Skip."
+fi
