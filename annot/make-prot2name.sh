1f4eb07ca590cc3d30963cfe85df704af37291e7
Taejoon Kwon <tkwon@unist.ac.kr>
2017-04-01 17:42:59 +0900

    Change for EnsEMBL species info

--- annot/make-prot2name.sh
+++ annot/make-prot2name.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 DIRNAME_ENS="/work/project/pub/ens/70"
-zgrep 'P0' $DIRNAME_ENS/*.gtf.gz | grep 'protein_coding' | awk -F"\t" '{print $9}' | awk -F";" '{print $4"\t"$7}' | sort -u > ens70.prot2name
-zgrep 'ENSP0' $DIRNAME_ENS/*.gtf.gz | grep 'protein_coding' | awk -F"\t" '{print $9}' | awk -F";" '{print $4"\t"$7}' | sort -u > ens70.HUMAN.prot2name
+echo ""> ens70.prot2name
+
+for FILENAME_GTF in $(ls $DIRNAME_ENS/*gtf.gz)
+do
+  SP_NAME=$(basename $FILENAME_GTF|awk -F"." '{print $1}')
+  echo $FILENAME_GTF $SP_NAME
+  zgrep 'P0' $FILENAME_GTF | grep 'protein_coding' | awk -F"\t" '{print $9}' | awk -v SP=$SP_NAME -F";" '{print $4"\t"$7"\t"SP}' | sort -u >> ens70.prot2name
+done
